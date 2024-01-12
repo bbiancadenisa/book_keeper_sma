e513baf359570dad595bc8bb5e351aa5bfa38a8d
biancadenisa <biancadenisa93@yahoo.com>
2024-01-12 11:08:35 +0200

    feat: display books added for user as well, add view favorites view

--- app/src/main/java/com/example/book_keeper/HomeUserActivity.kt
+++ app/src/main/java/com/example/book_keeper/HomeUserActivity.kt
@@ -6,10 +6,17 @@ import android.os.Bundle
 import com.example.book_keeper.databinding.ActivityHomeAdminBinding
 import com.example.book_keeper.databinding.ActivityHomeUserBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeUserBinding
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var booksArrayList: ArrayList<ModelBook>
+    private lateinit var adapterBooks: AdapterBooksAdmin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeUserBinding.inflate(layoutInflater)
@@ -17,12 +24,20 @@ class HomeUserActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadBooks()
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        binding.viewFavorites.setOnClickListener {
+            startActivity(Intent(this, ViewFavouritesActivity::class.java))
+            finish()
+        }
+
+
     }
 
     private fun checkUser() {
@@ -37,5 +52,32 @@ class HomeUserActivity : AppCompatActivity() {
             val email = firebaseUser.email
             binding.toolbarEmail.text = email
         }
+    }
+
+    private fun loadBooks() {
+
+        //initi arraylist
+        booksArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Books")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                booksArrayList.clear()
+                for (ds in snapshot.children) {
+                    val model = ds.getValue((ModelBook::class.java))
+                    booksArrayList.add(model!!)
+                }
+                adapterBooks = AdapterBooksAdmin(this@HomeUserActivity, booksArrayList)
+
+                //set adapter
+
+                binding.booksRv.adapter = adapterBooks
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
     }
 }
